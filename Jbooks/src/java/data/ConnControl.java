b28865f7574f28670db9c58c8b55c013fa171066
Nick <nickjrose@gmail.com>
2016-08-29 09:51:49 -0400

    Added comment on use of Class.forName() with JDBC

--- Jbooks/src/java/data/ConnControl.java
+++ Jbooks/src/java/data/ConnControl.java
@@ -11,12 +11,17 @@ public class ConnControl {
         Connection con = null;
         try {
             //define the connection data
-            String driverName = "com.mysql.jdbc.Driver"; //NB capital 'D' here is essential
+            String driverName = "com.mysql.jdbc.Driver"; //NB capital 'D' here
             String dbUrl = "";
             String userName = "";
             String password = "";
 
+            // This odd line is needed to create an instance of the JDBC Driver class and thus
+            // it's static block which registers the Driver itself. The return of this method
+            // isn't needed, so it isn't assigned anywhere.
+            // Refer to: http://stackoverflow.com/questions/2092659/what-is-the-difference-between-class-forname-and-class-forname-newinstanc
             Class.forName(driverName);
+            
             //open the connection
             con = DriverManager.getConnection(dbUrl, userName, password);
             return con;
